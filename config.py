c5e7e4e205f0d138609cb84bbeff4130d40a67c0
emiranivala <taitufam@gmail.com>
2025-02-16 19:19:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,15 +3,15 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", "23783378"))
-API_HASH = getenv("API_HASH", "1151425ad8d6fa61d47247f9ee841a37")
+API_ID = int(getenv("API_ID", "22642292"))
+API_HASH = getenv("API_HASH", "4502d35191a2fcb02c8467f54789f0ea")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "922270982").split()))
 MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "-1002292854905")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002363540243"))
-FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "50"))
-PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "50000000"))
+LOG_GROUP = getenv("LOG_GROUP", "-1002230414810")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002304203111"))
+FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "20"))
+PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
 WEBSITE_URL = getenv("WEBSITE_URL", None)
 AD_API = getenv("AD_API", None)
 STRING = getenv("STRING", None)
